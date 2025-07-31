bf1b59547b46a67823b850c7dd7def1cb5a4e380
Laura Viglioni <laura@vio.com>
2025-07-31 16:53:01 -0300

    fix: straigth load path

--- l.el
+++ l.el
@@ -154,9 +154,15 @@
 ;;
 ;;; Code:
 
-;; Adding ./lib to load-path
+;; Adding /lib to load-path
 (add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
 (add-to-list 'load-path (expand-file-name "lib" (file-name-directory (or load-file-name buffer-file-name))))
+
+;; Adding /lib path to straight
+(when (fboundp 'straight--repos-dir)
+  (add-to-list 'load-path
+               (straight--repos-dir "l-el/lib")))
+
 
 ;; internal library paths
 (require 'l-load-path)
