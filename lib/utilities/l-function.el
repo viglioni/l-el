b66529b5ebd2cb6b20b9e041a235ba4409fd9243
laura <lviglioni@canarytechnologies.com>
2025-11-25 18:34:38 -0300

    feat: added pipe

--- lib/utilities/l-function.el
+++ lib/utilities/l-function.el
@@ -31,6 +31,12 @@
 ;;; Code:
 
 (require 'l-main)
+(require 'cl-lib)
+(require 'seq)
+
+;;
+;; Function composition (rtl -- math order)
+;;
 
 (defmacro lcomp (&rest fns)
   "`lcomp' provides function composition right to left.
@@ -74,14 +80,75 @@ With local bindings:
   (with-l ((lcomp (+ __ 1) square) 3))) ;; => 10"
   `(l--comp ,@(mapcar (lambda (x) `(__ ,x)) fns)))
 
-
 @doc "Internal function for `lcomp'. Composes functions right to left."
 (ldef l--comp () (l x -> x))
+
 (ldef l--comp ((f :function)) f)
+
 (ldef l--comp ((f :function) (g :function))
       `(lambda (&rest args) (funcall (quote ,f) (apply (quote ,g) args))))
+
 (ldef l--comp ((f :function) (g :function) (fn-list :rest))
       (apply 'l--comp (l--comp f g) fn-list))
+
+
+;;
+;; Pipe function
+;;
+
+(defmacro lpipe (arg &rest fns)
+  "`lpipe' provides function piping left to right.
+
+Function piping allows you to pass a value through a series of transformations,
+where each function receives the result of the previous function. The piping
+is applied left to right, meaning the leftmost function is applied first.
+
+Each function argument is automatically wrapped with `__', so you can use the
+`__' placeholder syntax directly: (lpipe 5 (* __ 2) (+ __ 1)) instead of
+\(lpipe 5 (__ (* __ 2)) (__ (+ __ 1))).
+
+since: NEXT
+
+Arguments:
+- arg: The initial value to pipe through the functions
+- fns: Functions to apply in sequence, left to right
+
+Examples:
+Basic piping with lambdas:
+\(ldef double (l x -> (* 2 x)))
+\(ldef add-one (l x -> (+ 1 x)))
+\(lpipe 5 double add-one) ;; => 11
+
+Using __ placeholder syntax:
+\(lpipe 5 (* __ 2) (+ __ 1)) ;; => 11
+
+With `with-l' for cleaner syntax:
+\(with-l (lpipe 5 (* __ 2) (+ __ 1))) ;; => 11
+
+Multiple function piping:
+\(lpipe 10 (* __ 3) (* __ 2) (l x -> (+ x 1))) ;; => 61
+
+String processing example:
+\(lpipe \"  hello  \" string-trim upcase) ;; => \"HELLO\"
+
+With local bindings:
+\(let ((square (lambda (x) (* x x))))
+  (lpipe 3 square (+ __ 1))) ;; => 10
+
+Comparison with lcomp:
+lpipe and lcomp produce the same result but with reversed order.
+\(lpipe x f g h) is equivalent to (funcall (lcomp h g f) x)"
+  `(l--pipe ,arg ,@(mapcar (lambda (x) `(__ ,x)) fns)))
+
+
+@doc "Internal function for `lpipe'. Pipes value through functions left to right."
+(ldef l--pipe (arg) arg)
+
+(ldef l--pipe (arg (f :function)) (funcall f arg))
+
+(ldef l--pipe (arg (f :function) (fn-list :rest))
+      (apply #'l--pipe `(,(l--pipe arg f) ,@fn-list)))
+
 
 (provide 'l-function)
 ;;; l-function.el ends here
--- test/utilities/l-function-test.el
+++ test/utilities/l-function-test.el
@@ -89,4 +89,148 @@
 
     (test-it "uses __ multiple times in same expression"
       (expect (with-l ((lcomp (l x -> (* x 2)) (+ __ __)) 5))
-              :to-equal 20))))
+              :to-equal 20)))
+
+  (describe "lpipe basic usage"
+    (test-it "pipes value through single function"
+      (expect (lpipe 5 (lambda (x) (+ x 1))) :to-equal 6))
+
+    (test-it "pipes value through two functions left to right"
+      (let ((double (lambda (x) (* x 2)))
+            (add-ten (lambda (x) (+ x 10))))
+        (expect (lpipe 5 double add-ten) :to-equal 20))) ; (* 5 2) -> 10, (+ 10 10) -> 20
+
+    (test-it "pipes string through transformations"
+      (let ((add-exclamation (lambda (s) (concat s "!")))
+            (upcase-fn (lambda (s) (upcase s))))
+        (expect (lpipe "hello" upcase-fn add-exclamation) :to-equal "HELLO!"))))
+
+  (describe "lpipe with multiple functions"
+    (test-it "pipes through three numeric functions"
+      (let ((square (lambda (x) (* x x)))
+            (double (lambda (x) (* x 2)))
+            (add-one (lambda (x) (+ x 1))))
+        (expect (lpipe 3 square double add-one) :to-equal 19))) ; 3 -> 9 -> 18 -> 19
+
+    (test-it "pipes through string transformations"
+      (let ((trim-spaces (lambda (s) (string-trim s)))
+            (upcase-fn (lambda (s) (upcase s)))
+            (add-suffix (lambda (s) (concat s " Jr.")))
+            (add-prefix (lambda (s) (concat "Mr. " s))))
+        (expect (lpipe " john " trim-spaces upcase-fn add-suffix add-prefix)
+                :to-equal "Mr. JOHN Jr."))))
+
+  (describe "lpipe with type transformations"
+    (test-it "pipes through number to string to list"
+      (let ((multiply-by-ten (lambda (x) (* x 10)))
+            (to-string (lambda (n) (number-to-string n)))
+            (to-list (lambda (s) (list s))))
+        (expect (lpipe 5 multiply-by-ten to-string to-list) :to-equal '("50"))))
+
+    (test-it "pipes through list operations"
+      (let ((add-item (lambda (lst) (cons 'new-item lst)))
+            (get-length (lambda (lst) (length lst)))
+            (not-empty-p (lambda (n) (> n 0))))
+        (expect (lpipe '(a b) add-item get-length not-empty-p) :to-be t)))
+
+    (test-it "pipes through complex data transformations"
+      (let ((reverse-list (lambda (lst) (reverse lst)))
+            (add-pair (lambda (alist) (cons '(new . value) alist)))
+            (get-keys (lambda (alist) (mapcar 'car alist))))
+        (expect (lpipe '((a . 1) (b . 2)) reverse-list add-pair get-keys)
+                :to-equal '(new b a)))))
+
+  (describe "lpipe with __ placeholder syntax"
+    (test-it "pipes with __ expression"
+      (expect (lpipe 10 (exp __) (l x -> (1+ x)))
+              :to-equal (1+ (exp 10))))
+
+    (test-it "pipes with multiple __ functions"
+      (expect (lpipe 5 (exp __) (1+ __) (l x -> (* x 2)))
+              :to-equal (* 2 (1+ (exp 5)))))
+
+    (test-it "pipes with built-in functions using __"
+      (expect (lpipe 5 (* __ 10) (number-to-string __) (l x -> (concat "Result: " x)))
+              :to-equal "Result: 50"))
+
+    (test-it "pipes combining __ with regular lambdas"
+      (let ((square (lambda (x) (* x x))))
+        (expect (lpipe -3 (abs __) square (l x -> (+ x 1)))
+                :to-equal (1+ (* 3 3)))))
+
+    (test-it "pipes with list operations using __"
+      (expect (lpipe '(1 2 3 4) #'cdr (reverse __) (l x -> (length x)))
+              :to-equal (length (reverse (cdr '(1 2 3 4))))))
+
+    (test-it "pipes with __ used multiple times in expression"
+      (expect (lpipe 5 (+ __ __) (l x -> (* x 2)))
+              :to-equal 20)))
+
+  (describe "lpipe vs lcomp comparison"
+    (test-it "produces same result as lcomp but with reversed order"
+      (let ((f1 (lambda (x) (* x 2)))
+            (f2 (lambda (x) (+ x 10)))
+            (f3 (lambda (x) (- x 5))))
+        (expect (lpipe 5 f1 f2 f3) :to-equal (funcall (lcomp f3 f2 f1) 5))))
+
+    (test-it "lpipe evaluates left to right, lcomp right to left"
+      (expect (lpipe 3 (l x -> (* x 2)) (l x -> (+ x 10)))
+              :to-equal (funcall (lcomp (l x -> (+ x 10)) (l x -> (* x 2))) 3))))
+
+  (describe "lpipe edge cases"
+    (test-it "works with no functions (returns value unchanged)"
+      (expect (lpipe 42) :to-equal 42))
+
+    (test-it "works with identity function"
+      (expect (lpipe 10 (l x -> x)) :to-equal 10))
+
+    (test-it "works with nil as initial value"
+      (expect (lpipe nil (l x -> (or x 'default))) :to-equal 'default))
+
+    (test-it "works with complex nested data"
+      (let ((extract-name (lambda (person) (cdr (assq 'name person))))
+            (upcase-name (lambda (name) (upcase name))))
+        (expect (lpipe '((name . "john") (age . 30)) extract-name upcase-name)
+                :to-equal "JOHN"))))
+
+  (describe "lcomp error cases"
+    (test-it "raises error when passing non-function to composed result"
+      (expect (funcall (lcomp (l x -> (+ x 1)) 'not-a-function) 5)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing symbol that is not a function"
+      (expect (funcall (lcomp (l x -> (* x 2)) 'undefined-symbol) 10)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing number as function"
+      (expect (funcall (lcomp 42 (l x -> (+ x 1))) 5)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing string as function"
+      (expect (funcall (lcomp "not-a-function" (l x -> (+ x 1))) 5)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing list as function"
+      (expect (funcall (lcomp '(1 2 3) (l x -> (+ x 1))) 5)
+              :to-throw 'l-pattern-match-error)))
+
+  (describe "lpipe error cases"
+    (test-it "raises error when passing non-function"
+      (expect (lpipe 5 'not-a-function)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing symbol that is not a function"
+      (expect (lpipe 10 'undefined-symbol)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing number as function"
+      (expect (lpipe 5 42)
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing string as function"
+      (expect (lpipe 5 "not-a-function")
+              :to-throw 'l-pattern-match-error))
+
+    (test-it "raises error when passing list as function in middle of pipe"
+      (expect (lpipe 5 (l x -> (* x 2)) '(1 2 3) (l x -> (+ x 1)))
+              :to-throw 'l-pattern-match-error))))
